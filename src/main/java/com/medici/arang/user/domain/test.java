e70c1b69cc4211cc1ee2c0e635862f161ebd9888
Kei037 <juneway@hanmail.net>
2022-08-09 10:15:04 +0900

    gelgel

--- src/main/java/com/medici/arang/user/domain/test.java
+++ src/main/java/com/medici/arang/user/domain/test.java
@@ -3,6 +3,6 @@ package com.medici.arang.user.domain;
 public class test {
 	public String testCode;
 	public static void main(String[] args) {
-		System.out.println("glglgl");
+		System.out.println("gelgelgel");
 	}
 }
